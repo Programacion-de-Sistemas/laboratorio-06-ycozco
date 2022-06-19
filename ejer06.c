26eb0c7b050deeecdb9c1d09ffed19ac3f73501b
ycozco <ycozco@unsa.edu.pe>
2022-06-19 17:45:52 -0500

    test inicial

---
+++ ejer06.c
@@ -0,0 +1,44 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+    char** graySquare = reverse(whiteSquare);
+    char** casilla1 = join(graySquare,whiteSquare);
+    char** repeat_c1 = repeatH(casilla1,4);
+    char** rep_tab = repeatV(repeat_c1,2);
+    char** rook_knight = join(rook,knight);
+    char** bishop_queen = join(bishop,queen);
+    char** king_bishop = join(king,bishop);
+    char** knight_rook = join(knight,rook);
+    char** m1 = join(rook_knight,bishop_queen);
+    char** m2 = join(king_bishop,knight_rook);
+    char** m3 = join(m1,m2);
+
+    char** tab_01 = superImpose(m3,rep_tab);
+
+
+    char** graySquare = reverse(whiteSquare);
+    char** casilla1 = join(graySquare,whiteSquare);
+    char** repeat_c1 = repeatH(casilla1,4);
+    char** rep_tab = repeatV(repeat_c1,2);
+
+    char** rook_inv = reverse(rook);
+    char** knight_inv = reverse(knight);
+    char** bishop_inv = reverse(bishop);
+    char** queen_inv = reverse(queen);
+    char** king_inv = reverse(king);
+
+    char** rook_knight = join(rook_inv,knight_inv);
+    char** bishop_queen = join(bishop_inv,queen_inv);
+    char** king_bishop = join(king_inv,bishop_inv);
+    char** knight_rook = join(knight_inv,rook_inv);
+    char** m1 = join(rook_knight,bishop_queen);
+    char** m2 = join(king_bishop,knight_rook);
+    char** m3 = join(m1,m2);
+
+    char** tab_04 = superImpose(m3,rep_tab);
+
+
+    char** tabla_general = up(tab_01,tab_04);
+    interpreter(tabla_general);
+}

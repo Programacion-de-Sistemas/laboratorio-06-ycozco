ea7e9cce9116d24e3517fa92cb0804df4eee47a6
ycozco <ycozco@unsa.edu.pe>
2022-06-19 16:39:51 -0500

    logica puesta, resultado no funciona core dumped error

--- ejer04.c
+++ ejer04.c
@@ -3,6 +3,16 @@
 
 void display(){
   char** two_combo = join(graySquare,whiteSquare);
-  char** piece_01 = superImpose(knight,graySquare);
-  interpreter(piece_01);
+  char** combined= repeatH(two_combo,4); 
+
+  char** rook_knight = join(rook,knight);
+  char** bishop_queen = join(bishop,queen);
+  char** king_bishop = join(king,bishop);
+  char** knight_rook = join(knight,rook);
+  char** m1 = join(rook_knight,bishop_queen);
+  char** m2 = join(king_bishop,knight_rook);
+  char** m3 = join(m1,m2);
+  char** imposed = superImpose(m3,combined);
+  interpreter(imposed);
+
 }

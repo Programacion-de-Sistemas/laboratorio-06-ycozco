0110b8e8c4a9b38a39424424f983917fea7481a7
ycozco <ycozco@unsa.edu.pe>
2022-06-19 17:18:05 -0500

    reutilizando el codigo de una prueba anterior, puesta en notas, realizo una doble creacion de casilleros, (gris-blanco;blanco-gris) una doble repeticion en estos  mismos de manera que el error fue suprimido

--- ejer04.c
+++ ejer04.c
@@ -4,14 +4,24 @@
 void display(){
 
 
-    char** test1 = join(graySquare,whiteSquare);
-    char** test2 = join(rook,knight);
-    char** impose1 = superImpose(test1,test1);
-    char** impose2 = superImpose(test2,test2);
-    char** ip = superImpose(rook,rook);
-    char** impose3 = superImpose(impose1,ip);
+    char** casilla1 = join(graySquare,whiteSquare);
+    char** repeat_c1 = repeatH(casilla1,4);
 
+    char** casilla2 = join(whiteSquare,graySquare);
+    char** repeat_c2 = repeatH(casilla2,4);
 
-  interpreter(impose3);
+    char** up_c = up(repeat_c1,repeat_c2);
+    char** rep_tab = repeatV(up_c,2);
+
+  char** rook_knight = join(rook,knight);
+  char** bishop_queen = join(bishop,queen);
+  char** king_bishop = join(king,bishop);
+  char** knight_rook = join(knight,rook);
+  char** m1 = join(rook_knight,bishop_queen);
+  char** m2 = join(king_bishop,knight_rook);
+  char** m3 = join(m1,m2);
+    char** tab_01 = superImpose(m3,rep_tab);
+    interpreter(tab_01);
+
 
 }

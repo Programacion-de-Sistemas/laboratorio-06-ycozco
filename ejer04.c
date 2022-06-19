fec4809839ed86558dc9a996c8fbe871fc0a6377
ycozco <ycozco@unsa.edu.pe>
2022-06-19 17:00:45 -0500

    varias formas de hacer super Impose y sigue el mismo error

--- ejer04.c
+++ ejer04.c
@@ -2,17 +2,16 @@
 #include "figures.h"
 
 void display(){
-  char** two_combo = join(graySquare,whiteSquare);
-  char** combined= repeatH(two_combo,4); 
 
-  char** rook_knight = join(rook,knight);
-  char** bishop_queen = join(bishop,queen);
-  char** king_bishop = join(king,bishop);
-  char** knight_rook = join(knight,rook);
-  char** m1 = join(rook_knight,bishop_queen);
-  char** m2 = join(king_bishop,knight_rook);
-  char** m3 = join(m1,m2);
-  char** imposed = superImpose(m3,combined);
-  interpreter(imposed);
+
+    char** test1 = join(graySquare,whiteSquare);
+    char** test2 = join(rook,knight);
+    char** impose1 = superImpose(test1,test1);
+    char** impose2 = superImpose(test2,test2);
+    char** ip = superImpose(rook,rook);
+    char** impose3 = superImpose(impose1,ip);
+
+
+  interpreter(impose3);
 
 }

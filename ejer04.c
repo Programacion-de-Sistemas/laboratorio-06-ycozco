f0c2cbb28cfe9cc7faba935309faf84bdd0b29de
ycozco <ycozco@unsa.edu.pe>
2022-06-19 17:21:40 -0500

    haciendo pruebas, llegue a la conclusion que el repeatV sobre la primera fila que eh creado soluciona de manera mas eficiente mi error

--- ejer04.c
+++ ejer04.c
@@ -6,20 +6,16 @@ void display(){
 
     char** casilla1 = join(graySquare,whiteSquare);
     char** repeat_c1 = repeatH(casilla1,4);
+    char** rep_tab = repeatV(repeat_c1,2);
 
-    char** casilla2 = join(whiteSquare,graySquare);
-    char** repeat_c2 = repeatH(casilla2,4);
+    char** rook_knight = join(rook,knight);
+    char** bishop_queen = join(bishop,queen);
+    char** king_bishop = join(king,bishop);
+    char** knight_rook = join(knight,rook);
+    char** m1 = join(rook_knight,bishop_queen);
+    char** m2 = join(king_bishop,knight_rook);
+    char** m3 = join(m1,m2);
 
-    char** up_c = up(repeat_c1,repeat_c2);
-    char** rep_tab = repeatV(up_c,2);
-
-  char** rook_knight = join(rook,knight);
-  char** bishop_queen = join(bishop,queen);
-  char** king_bishop = join(king,bishop);
-  char** knight_rook = join(knight,rook);
-  char** m1 = join(rook_knight,bishop_queen);
-  char** m2 = join(king_bishop,knight_rook);
-  char** m3 = join(m1,m2);
     char** tab_01 = superImpose(m3,rep_tab);
     interpreter(tab_01);
 

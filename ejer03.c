d8c546ba2e63bcdf7f3afd40d0aa70229e51d44d
ycozco <ycozco@unsa.edu.pe>
2022-06-19 16:13:43 -0500

    corrigiendo el repeatH para normal y reversed, se agrega un repeat x4 para establecer cada linea, tanto para reversed como para normal, luego se une, y un repeatV para unirlos

--- ejer03.c
+++ ejer03.c
@@ -4,8 +4,10 @@
 void display(){
 
   char** normal = join(graySquare,whiteSquare);
+  char** normal2 = repeatH(normal,4);
   char** reversed = reverse(normal);
-  char** result= up(normal,reversed); 
+  char** reversed2 = repeatH(reversed,4);
+  char** result= up(normal2,reversed2); 
   char** result2 = repeatV(result,2);
   interpreter(result2);
 }
